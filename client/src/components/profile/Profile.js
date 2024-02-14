3ab114928a4bc2d9bd829ccf16b4a45c139ea4aa
bhargav-kalsariya <kalsariyabhargav187@gmail.com>
2024-02-14 16:53:19 +0530

    create video api integresions done

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,13 +1,51 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import './Profile.scss'
+import { axiosClient } from './../../utility/axiosClient';
 import img from './../../images/1692860352616.jpg'
 
 function Profile() {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const titleRef = useRef(null);
+    const descRef = useRef(null);
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        setTitle(titleRef.current);
+        setDescription(descRef.current);
+
+        const response = await axiosClient.post('/video/create', {
+            title,
+            description
+        });
+
+        return console.log(response);
+
+    }
+
     return (
         <div className='Profile'>
             <div className="profile-wrapper">
                 <div className="left-part">
-                    left
+                    <div className="create-video">
+                        <div className="video-part">
+                            video comes here
+                        </div>
+                        <div className="video-data-part">
+                            <label htmlFor="title"></label>
+                            <input type="text" id="title" ref={titleRef} />
+                            <label htmlFor="description"></label>
+                            <input type="text" id="description" ref={descRef} />
+                            <button onClick={handleSubmit}>Create video</button>
+                        </div>
+                    </div>
+                    <h3>My videos</h3>
+                    <div className="your-videos">
+
+                    </div>
                 </div>
                 <div className="right-part">
                     <div className="profile-box">
